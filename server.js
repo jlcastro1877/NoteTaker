503a962ac5f523c3e8233cc043a89737d922472c
jlcastro1877 <jlcastro1877@gmail.com>
2024-05-20 14:04:49 -0600

    migrating to ejs

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+const path = require("path");
 const express = require("express");
-const html_routes = require("./routes/html-routes");
-const api_routes = require("./routes/api-routes");
-const PORT = process.env.PORT || 3000;
-// dynamically set the port
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-// Express middleware will always run the operation in the order from top to bottom "order matters"
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(express.static("public"));
-app.use(html_routes);
-app.use(api_routes);
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res, next) => {
+  res.render("index.ejs");
+});
+
+app.get("/notes", (req, res, next) => {
+  res.render("notes.ejs");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
